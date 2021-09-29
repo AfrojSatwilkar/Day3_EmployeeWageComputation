5453d29e4a549b3a747b0275287ce812c46490f4
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-09-29 23:59:20 +0530

    Daily Employee Wage Calculator

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -8,12 +8,17 @@ public class EmployeeWageComputation {
 
         System.out.println("Welcome to Employee Wage Computation Program using OOP");
         int IS_PRESENT = 1;
+        int WAGE_PER_HR = 20;
+        int EMP_HRS;
+        int TotalWageInDay;
         Random num = new Random();
         int randomCheck = num.nextInt(2);
         if (IS_PRESENT == randomCheck)
-            System.out.println("Employee is Present");
+            EMP_HRS = 8;
         else
-            System.out.println("Employee is Absent");
+            EMP_HRS = 0;
+        TotalWageInDay = WAGE_PER_HR * EMP_HRS;
+        System.out.println("Total wage in Day : "+TotalWageInDay);
 
     }
 }
