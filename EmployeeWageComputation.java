01747d30c883cd9564f9df30b764f9ea09aa5e93
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-09-29 23:35:02 +0530

    Welcome to Employee Wage Computation

---
+++ EmployeeWageComputation.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class EmployeeWageComputation {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee Wage Computation Program using OOP");
+
+    }
+}
