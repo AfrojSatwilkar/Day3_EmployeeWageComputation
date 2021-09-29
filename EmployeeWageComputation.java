76f3bc2abeb24428d1d4008021717549ba30c49b
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-09-30 00:34:51 +0530

    Calculate Wages for a Month

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -8,18 +8,24 @@ public class EmployeeWageComputation {
 
         System.out.println("Welcome to Employee Wage Computation Program using OOP");
         int WAGE_PER_HR = 20;
+        int MAX_WORKING_DAYS = 20;
+        int TOTAL_WORKING_DAYS = 0;
+        int TOTAL_EMP_HRS = 0;
         int EMP_HRS;
-        int TotalWageInDay;
-        Random num = new Random();
-        int randomCheck = num.nextInt(3);
-        switch (randomCheck){
-            case 1 -> EMP_HRS=8;
-            case 2 -> EMP_HRS=4;
-            default -> EMP_HRS=0;
-
+        int TOTAL_SALARY;
+        while (TOTAL_WORKING_DAYS < MAX_WORKING_DAYS){
+            Random num = new Random();
+            int randomCheck = num.nextInt(3);
+            switch (randomCheck) {
+                case 1 -> EMP_HRS = 8;
+                case 2 -> EMP_HRS = 4;
+                default -> EMP_HRS = 0;
+            }
+            TOTAL_EMP_HRS = TOTAL_EMP_HRS + EMP_HRS;
+            TOTAL_WORKING_DAYS++;
         }
-        TotalWageInDay = WAGE_PER_HR * EMP_HRS;
-        System.out.println("Total wage in Day : "+TotalWageInDay);
+        TOTAL_SALARY = WAGE_PER_HR * TOTAL_EMP_HRS;
+        System.out.println("Total Salary : "+TOTAL_SALARY);
 
     }
 }
